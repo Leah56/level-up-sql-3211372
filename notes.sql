6f7e29e0a00091a373139a3d7112f4414f912f00
Leah56 <47418146+Leah56@users.noreply.github.com>
2024-09-03 16:01:16 +0000

    updated with the next challenge

--- notes.sql
+++ notes.sql
@@ -10,7 +10,11 @@ SELECT * FROM Customers;
 --8/8/24 Provide a list of customers first names, last names, and emails in alphabetical order by last name.
 SELECT FirstName, LastName, Email FROM Customers ORDER BY LastName ASC;
 
+--9/3/24 Create a report showing how many books were published each year (with only one copy of books that there are multiples of),
+-- and the years with the most titles published at the top of the list.
+-- Create a second report that shows the 5 book titles that have been checked out the most.
 
+SELECT * FROM Books;
 
 
 
