9c2ff2211ece767e682ee5a86c2dd00bcdc55be9
Leah56 <47418146+Leah56@users.noreply.github.com>
2024-09-12 20:38:45 +0000

    finished the customer loyalty program challenge

--- notes.sql
+++ notes.sql
@@ -8,9 +8,45 @@ SELECT * FROM Dishes;
 SELECT * FROM Customers;
 
 --8/8/24 Provide a list of customers first names, last names, and emails in alphabetical order by last name.
-SELECT FirstName, LastName, Email FROM Customers ORDER BY LastName ASC;
+SELECT FirstName, LastName, Email 
+FROM Customers 
+ORDER BY LastName ASC;
 
---9/3/24 Create a report showing how many books were published each year (with only one copy of books that there are multiples of),
+
+
+--9/3/24 Create a table to store new info about customers (how many people they'll have in their party)
+CREATE TABLE rsvps (
+rsvps_CustomerID INTEGER REFERENCES Customers(CustomerID),
+party_size INTEGER
+);
+
+INSERT INTO rsvps VALUES (1, 2);
+SELECT * FROM rsvps;
+DELETE FROM rsvps WHERE rsvps_CustomerID=1;
+
+
+--Create three menus: 1. all items sorted by price lowest to highest 
+-- 2. only appetizers and beverages (sorted by type of dish)
+-- 3. all items except beverages (sorted by type of dish)
+SELECT Type, Name, Price, Description FROM Dishes
+ORDER BY Price ASC;
+
+SELECT Type, Name, Price, Description FROM Dishes
+WHERE Type='Appetizer' OR Type='Beverage'
+ORDER BY Type ASC;
+
+SELECT Type, Name, Price, Description FROM Dishes
+WHERE Type !='Beverage'
+ORDER BY Type ASC;
+
+--"Sign a customer up for a loyalty program" (Difficulty 2)
+--Add customer info to the customers table (INSERT) to the specific columns (don't have to add all the columns)
+INSERT INTO Customers (FirstName, LastName, Email, Address, City, State, Phone, Birthday) VALUES ('Anna', 'Smith','asmith@samoca.org', '479 Lapis Dr.', 'Memphis', 'TN', '555-555-1212', '1973-07-21');
+SELECT * FROM Customers
+ORDER BY CustomerID DESC;
+
+
+-- Create a report showing how many books were published each year (with only one copy of books that there are multiples of),
 -- and the years with the most titles published at the top of the list.
 -- Create a second report that shows the 5 book titles that have been checked out the most.
 
